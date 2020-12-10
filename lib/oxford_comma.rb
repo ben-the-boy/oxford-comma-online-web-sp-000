461416ef7e521dc15bce71806028c59114014e8d
Ben Hayes <b.t.hayes@comcast.net>
2020-12-10 23:46:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
     elsif array.size == 2 
     return array.join(" and ")
     elsif array.size >= 3
-    return array.join(" , ")
+    return array.join(" , ") <<   
   end
 end
